eeeec48f83e9f3ed1c513ed9acaaeb95738f4d80
Morgane Avi <morgane.bonillo@hotmail.fr>
2025-02-17 21:22:27 +0100

    done

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: morgane <morgane@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 21:02:27 by morgane           #+#    #+#             */
-/*   Updated: 2025/02/17 21:07:32 by morgane          ###   ########.fr       */
+/*   Updated: 2025/02/17 21:22:06 by morgane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,30 @@
 #include <iostream>
 #include <string>
 
-int main(int ac, char **av) {
+int main(int ac, char **av)
+{
 
-    int i;
-    int j;
+	int i;
+	int j;
 
-    i = 0;
-    if (ac == 1)
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" <<std::endl;
-    while (i < ac)
-    {
-        j = 0;
-        while(ac[i][j])
-        {
-            std::cout << (char)toupper(ac[i][j]);
-            j++;
-        }
-        i++;
-        if (i)
-            std::cout << " ";
-    }
-    std::cout << std::endl;
-    return 0;
+	i = 1;
+	if (ac == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return 0;
+	}
+	while (i < ac)
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			std::cout << (char)toupper(av[i][j]);
+			j++;
+		}
+		i++;
+		if (i < ac)
+			std::cout << " ";
+	}
+	std::cout << std::endl;
+	return 0;
 }
