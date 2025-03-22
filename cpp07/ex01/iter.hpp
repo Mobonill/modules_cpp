aaa5fdc18cb4417d94338cf66e00cd9475587fd2
Morgane Avi <morgane.bonillo@hotmail.fr>
2025-03-22 15:34:32 +0100

    ex01 finished

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -3,15 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   iter.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mobonill <mobonill@student.42.fr>          +#+  +:+       +#+        */
+/*   By: morgane <morgane@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 15:38:32 by mobonill          #+#    #+#             */
-/*   Updated: 2025/03/20 16:05:07 by mobonill         ###   ########.fr       */
+/*   Updated: 2025/03/22 14:42:30 by morgane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
 #include <iostream>
 #include <string>
+#include <stdio.h>
+#include <ctype.h>
 
-// template <typename T>
-// void iter(T& tab, T& sizeof(tab), T&  ) 
+template <typename T>
+void iter(T* tab, int length, void (*function)(T&)){
+    for (int i = 0; i < length; i++)
+        function(tab[i]);
+}
--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -3,27 +3,46 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mobonill <mobonill@student.42.fr>          +#+  +:+       +#+        */
+/*   By: morgane <morgane@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 15:39:02 by mobonill          #+#    #+#             */
-/*   Updated: 2025/03/20 16:11:30 by mobonill         ###   ########.fr       */
+/*   Updated: 2025/03/22 14:41:46 by morgane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 
+
+void upperCase(std::string& str) {
+	
+	for(size_t i = 0; i < str.length(); i++)
+		str[i] = toupper(str[i]);
+}
+
+void square(int& n) {
+	n = n * n;
+}
+
+
 int main(void) {
 
-	std::string array[] = {
-		array[0] = "Hello",
-		array[1] = "there",
-		array[2] = "tadatadaaaa daaam",
-		array[3] = " !"
+	int tab[4] = {1, 2, 3, 4};
+
+	std::cout << "BEFORE ---> " << tab[0] << ", " << tab[1] << ", " << tab[2] << ", " << tab[3] << std::endl;
+	iter(tab, 4, square);
+	std::cout << "AFTER ---> " << tab[0] << ", " << tab[1] << ", " << tab[2] << ", " << tab[3] << std::endl << std::endl;
+
+	
+
+	std::string str_tab[3] = {
+		"Hello",
+		"There!",
+		"Obiwan Kenobi"
 	};
-	
-	// array->size()
-	sizeof(array) = int k;
-	std::cout << k <<std::endl;
-	
+
+	std::cout << "BEFORE ---> " << str_tab[0] << " " << str_tab[1] << " "  << str_tab[2] << std::endl;
+	iter(str_tab, 3, upperCase);
+	std::cout << "AFTER ----> " << str_tab[0] << " " << str_tab[1] << " "  << str_tab[2] << std::endl;
+
 	return 0;
 }
